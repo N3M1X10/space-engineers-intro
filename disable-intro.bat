a9056addaa8c3389030b27ca97202289b7e93242
N3M1X10 <nemixmail@gmail.com>
2024-10-23 19:26:38 +0500

    repo create

---
+++ disable-intro.bat
@@ -0,0 +1,38 @@
+@echo off
+chcp 65001 >nul
+title Disable Space Engineers intro
+
+:: PLACE THIS INTO THE GAME ROOT FOLDER
+
+::intro video path
+set intro=%~dp0Content\Videos\KSH.wmv
+
+IF EXIST=="%~dp0Content\Videos\-disabled-KSH.wmv" (
+echo ! Интро уже отключено
+goto end
+)
+
+IF EXIST=="%intro%" (
+goto :run
+) else (
+echo ! Не удалось найти файл интро
+echo ! Убедитесь что запускаемый файл находится в корневой папке игры Space Engineers
+goto end
+)
+
+:run
+::rename the video (with checking of existing this filename)
+
+>nul ren "%intro%" "-disabled-KSH.wmv"
+
+echo Интро больше вас не побеспокоит!
+echo.
+echo Здесь лежит переименованное видео из игры, что отвечает за интро:
+echo "%~dp0Content\Videos\-disabled-KSH.wmv"
+goto end
+
+
+:end
+echo.
+echo Нажмите любую кнопку чтобы закрыть окно
+pause >nul
---
+++ restore-intro.bat
@@ -0,0 +1,29 @@
+@echo off
+chcp 65001 >nul
+title Restore Space Engineers Intro
+
+:: PLACE THIS INTO THE GAME ROOT FOLDER
+
+::intro video path
+set intro=%~dp0Content\Videos\-disabled-KSH.wmv
+
+IF EXIST=="%intro%" (
+goto :run
+) else (
+echo ! Не удалось найти переименованный файл интро
+echo ! Убедитесь что запускаемый файл находится в корневой папке игры Space Engineers
+goto end
+)
+
+
+:run
+>nul ren "%intro%" "KSH.wmv"
+echo Интро восстановлено
+echo.
+echo Путь к интро: "%~dp0Content\Videos\KSH.wmv"
+
+
+:end
+echo.
+echo Нажмите любую кнопку чтобы закрыть окно
+pause >nul
